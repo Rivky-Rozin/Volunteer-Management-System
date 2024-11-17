bd9fdcbbaacf3ca5bb0ca4aab2d9c832ee30cedc
Rivky Rozin <r0583284970@gmail.com>
2024-11-17 21:39:03 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program1652.cs
@@ -0,0 +1,21 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome1652();
+            Welcome9845();
+            Console.ReadKey();
+        }
+
+        private static void Welcome1652()
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, Welcome to my first console application", name);
+        }
+        static partial void Welcome9845();
+    }
+}
+
