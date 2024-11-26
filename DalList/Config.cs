8e52a24de969cde8e9e98570da093fc57bf11f61
Rivky Rozin <r0583284970@gmail.com>
2024-11-26 17:14:41 +0200

    Volunteer class

---
+++ DalList/Config.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Reflection.Metadata;
+
+namespace Dal;
+internal static class Config
+{
+    
+    internal const int StartCallId = 0;
+    private static int NextCallId = StartCallId;
+    public static int NEXTCALLID { get { int temp = NextCallId; NextCallId++; return temp; } }
+    
+    internal const int StartAssignmentId = 0;
+    private static int NextAssignmentId = StartAssignmentId;
+    public static int NEXTASSIGNMENTID { get { int temp = NextAssignmentId; NextAssignmentId++; return temp; } }
+
+    static DateTime Clock;
+    static TimeSpan RiskRange;
+}
