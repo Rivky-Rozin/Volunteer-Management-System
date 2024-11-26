862059654ce03c2f5176501d5eff9ec31d352c58
bashy yacob <bashy3309@gmail.com>
2024-11-26 14:19:25 +0200

    record Assignment

---
+++ DalFacade/DO/Assignment.cs
@@ -0,0 +1,27 @@
+﻿
+namespace DO;
+/// <summary>
+/// Default constructor for the Assignment entity.
+/// Initializes all properties with default values.
+/// </summary>
+/// <param name="callId">Unique ID of the call.</param>
+/// <param name="volunteerId">Unique ID of the volunteer.</param>
+/// <param name="startTreatment">Start time of the treatment.</param>
+/// <param name="endTreatment">End time of the treatment (nullable).</param>
+/// <param name="treatmentType">The type of treatment's conclusion.</param>
+public record Assignment
+(
+   int Id,
+   int VolunteerId,
+   int CallId,
+   DateTime StartTreatment ,
+   DateTime? EndTreatment=null ,
+   Enum? TreatmentType=null
+
+)
+{
+    public Assignment()
+    : this(0, 0, 0,DateTime.Now, DateTime.Now, default) { }
+}
+
+
