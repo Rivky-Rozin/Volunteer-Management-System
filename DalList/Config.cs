3fffde90d6925fd6d936e30114eb145de4ffdb31
bashy yacob <bashy3309@gmail.com>
2024-11-26 17:21:52 +0200

    cange config

--- DalList/Config.cs
+++ DalList/Config.cs
@@ -6,9 +6,10 @@ namespace Dal;
 internal static class Config
 {
     
-    internal const int StartCallId = 0;
-    private static int NextCallId = StartCallId;
-    public static int NEXTCALLID { get { int temp = NextCallId; NextCallId++; return temp; } }
+
+    internal const int startCallId = 1000;
+    private static int nextCallId = startCallId;
+    public static int NextCallId { get => nextCallId++; }
     
     internal const int StartAssignmentId = 0;
     private static int NextAssignmentId = StartAssignmentId;
