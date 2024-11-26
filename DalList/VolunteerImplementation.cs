c672dd0fbaebdf4a8b38e3764153302339e51306
Rivky Rozin <r0583284970@gmail.com>
2024-11-26 21:09:44 +0200

    creating the implements for the interfaces without Implementing the methods

--- DalList/VolunteerImplementation.cs
+++ DalList/VolunteerImplementation.cs
@@ -1,40 +1,56 @@
 ﻿
 
 namespace Dal;
-
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using DalApi;
 using DO;
 
 public class VolunteerImplementation : IVolunteer
 {
+    //הוספת מתנדב חדש
     public void Create(Volunteer item)
     {
-        throw new NotImplementedException();
+        if (!(Read(item.Id) == null))
+        {
+            throw new Exception("A volunteer with this ID already exists");
+        }
+        DataSource.Volunteers.Add(item);
     }
 
+    //מחיקת משתמש בודד
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        if (Read(id)==null)
+        {
+            throw new Exception("A volunteer with this ID does not exist");
+        }
+        DataSource.Volunteers.Remove(Read(id));
     }
 
+    //מחיקת רשימת המתנדבים
     public void DeleteAll()
     {
-        throw new NotImplementedException();
+        DataSource.Volunteers.Clear();
     }
 
+    //קבלת מתנדב בודד
     public Volunteer? Read(int id)
     {
-        throw new NotImplementedException();
+        Volunteer? found;
+        found = DataSource.Volunteers.Find(vol => vol.Id == id);
+        return found;
     }
 
+    //קבלת עותק של הרשימה של כל המתנדבים
     public List<Volunteer> ReadAll()
     {
-        throw new NotImplementedException();
+        return new List<Volunteer>(DataSource.Volunteers);
     }
 
     public void Update(Volunteer item)
     {
-        throw new NotImplementedException();
+        Delete(item.Id);
+        Create(item);
     }
 }
