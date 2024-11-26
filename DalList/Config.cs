3afa4876736b6a3bde5fd65d81a3a37551a2221f
Rivky Rozin <r0583284970@gmail.com>
2024-11-26 20:22:43 +0200

    fixing config

--- DalList/Config.cs
+++ DalList/Config.cs
@@ -6,15 +6,28 @@ namespace Dal;
 internal static class Config
 {
     
-
+    //מספור רץ למחלקת הקריאה
     internal const int startCallId = 1000;
     private static int nextCallId = startCallId;
     public static int NextCallId { get => nextCallId++; }
     
+    //מספור רץ למחלקת השמה
     internal const int StartAssignmentId = 0;
     private static int nextAssignmentId = StartAssignmentId;
     public static int NextAssignmentId { get => nextAssignmentId++;  }
 
-    static DateTime Clock;
+    //שעון המערכת
+    internal static DateTime Clock { get; set; } = DateTime.Now;
+
+    //??? עדיין לא למדנו מה זה
+    //כאן צריך להיות אתחול השדה timespan
     static TimeSpan RiskRange;
+    internal static void Reset()
+    {
+        nextCallId = startCallId;
+        Clock = DateTime.UtcNow;    
+        Clock = DateTime.Now;
+        //כאן צריך להיות אתחול השדה timespan
+    }
+
 }
