275b6b14ec122ce78c97e55c1ff19a70795a6888
bashy yacob <bashy3309@gmail.com>
2025-04-23 21:09:11 +0300

    Make CallInProgress properties publicly accessible
    Updated the `CallInProgress` class in the `BO` namespace to change
    the access modifier of several properties (`CallType`, `OpenTime`,
    `MaxResolutionTime`, `EntryToTreatmentTime`, and `status`) from
    private to public. This allows these properties to be accessed
    and modified externally.

--- BL/BO/CallInProgress.cs
+++ BL/BO/CallInProgress.cs
@@ -6,20 +6,20 @@ public class CallInProgress
 {
     public int Id { get; init; }
     public int CallId { get; set; }
-    CallType CallType { get; set; }
+    public CallType CallType { get; set; }
     public string? Description { get; set; }
     public string FullAddress { get; set; }
     //זמן פתיחה
-    DateTime? OpenTime { get; set; }
+    public DateTime? OpenTime { get; set; }
     //זמן מקסימלי לסיום הקריאה
-    DateTime? MaxResolutionTime { get; set; }
+    public DateTime? MaxResolutionTime { get; set; }
     //זמן כניסה לטיפול
-    DateTime? EntryToTreatmentTime { get; set; }
+    public DateTime? EntryToTreatmentTime { get; set; }
 
     //מרחק קריאה מהמתנדב המטפל
     public double DistanceFromVolunteer { get; set; }
 
-    Status status { get; set; }
+    public Status status { get; set; }
 
 
 
