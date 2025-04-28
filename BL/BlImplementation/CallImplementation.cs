b9e8d84358300e7a51a477cc62287d5af546c939
Rivky Rozin <r0583284970@gmail.com>
2025-04-28 22:50:36 +0300

    Add initial BL implementations and update IConfig interface
    Introduced `AdminImplementation`, `CallImplementation`, and
    `VolunteerImplementation` classes in the `BlImplementation`
    namespace, implementing `IAdmin`, `ICall`, and `IVolunteer`
    interfaces respectively. Methods in `CallImplementation` and
    `VolunteerImplementation` are placeholders throwing
    `NotImplementedException`.
    
    Extended the `IConfig` interface in the `DalApi` namespace to
    include a new `RiskTimeSpan` property of type `TimeSpan`.
    
    Removed the `<ItemGroup>` entry for the `BlImplementation`
    folder from the `BL.csproj` file.

---
+++ BL/BlImplementation/CallImplementation.cs
@@ -0,0 +1,65 @@
+﻿
+namespace BlImplementation;
+
+using System.Collections.Generic;
+using BlApi;
+using BO;
+
+internal class CallImplementation : ICall
+{
+    private readonly DalApi.IDal _dal = DalApi.Factory.Get;
+    public void AddCall(Call call)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CancelCallTreatment(int requesterId, int assignmentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CompleteCallTreatment(int volunteerId, int assignmentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteCall(int callId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Call GetCallDetails(int callId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<CallInList> GetCallList(CallInList? filterField, object? filterValue, CallInList? sortField)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int[] GetCallStatusCounts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<ClosedCallInList> GetClosedCallsOfVolunteer(int volunteerId, CallType? callTypeFilter, ClosedCallInList? sortField)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<OpenCallInList> GetOpenCallsForVolunteer(int volunteerId, CallType? callTypeFilter, OpenCallInList? sortField)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SelectCallForTreatment(int volunteerId, int callId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateCall(Call call)
+    {
+        throw new NotImplementedException();
+    }
+}
