af3012d08ac3e45c2513c29802a6aeb2c1b0c078
bashy yacob <bashy3309@gmail.com>
2025-04-29 09:21:52 +0300

    Refactor CallManager to be static and add IDal field
    Updated `using` directives to include `DalApi`. Changed the
    `CallManager` class from `internal` to `internal static`.
    Added a private static `s_dal` field of type `IDal`,
    initialized using `Factory.Get`.

--- BL/Helpers/CallManager.cs
+++ BL/Helpers/CallManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DalApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Helpers
 {
-    internal class CallManager
+    internal static class CallManager
     {
-    }
+        private static IDal s_dal = Factory.Get; //stage 4
+    }  
 }
