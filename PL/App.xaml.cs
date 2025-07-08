011edeac2019b2f2e76564cc9da81c9cd7af1656
bashy yacob <bashy3309@gmail.com>
2025-07-08 12:59:45 +0300

    שינויים בעיקר בעיצוב

--- PL/App.xaml.cs
+++ PL/App.xaml.cs
@@ -9,6 +9,37 @@ namespace PL
     /// </summary>
     public partial class App : Application
     {
-    }
+        private static bool isDarkTheme = false;
 
+        public static void ThemeSwitchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeTheme();
+        }
+        public static void ChangeTheme()
+        {
+            // נקה את מילון המשאבים הקיים
+            Current.Resources.MergedDictionaries.Clear();
+
+            // צור מילון משאבים חדש
+            ResourceDictionary resourceDict = new ResourceDictionary();
+
+            if (isDarkTheme)
+            {
+                // אם כרגע כהה, שנה לבהיר
+                resourceDict.Source = new Uri("Themes/LightTheme.xaml", UriKind.Relative);
+                isDarkTheme = false;
+            }
+            else
+            {
+                // אם כרגע בהיר, שנה לכהה
+                resourceDict.Source = new Uri("Themes/DarkTheme.xaml", UriKind.Relative);
+                isDarkTheme = true;
+            }
+
+            // טען את מילון המשאבים החדש לתוך האפליקציה
+            Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+    }
 }
+
+
