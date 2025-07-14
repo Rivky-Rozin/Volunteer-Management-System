75a7e783d27c21cd16a2d34ec6f318656a375dc4
bashy yacob <bashy3309@gmail.com>
2025-07-14 16:46:59 +0300

    הוספת נושא רקע כהה

---
+++ PL/Controls/ThemeToggleButton.xaml.cs
@@ -0,0 +1,19 @@
+﻿// Controls/ThemeToggleButton.xaml.cs
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PL.Controls
+{
+    public partial class ThemeToggleButton : UserControl
+    {
+        public ThemeToggleButton()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeTheme_Click(object sender, RoutedEventArgs e)
+        {
+            App.ChangeTheme();
+        }
+    }
+}
